b69f954dd9f03acd8ff01cd42c0b21027e3ee427
Your Name <you@example.com>
2020-02-06 12:33:07 -0800

    refactor to include a base class of AbstractMelonOrder

--- melons.py
+++ melons.py
@@ -1,17 +1,7 @@
 """Classes for melon orders."""
 
-
-class DomesticMelonOrder():
-    """A melon order within the USA."""
-
-    def __init__(self, species, qty):
-        """Initialize melon order attributes."""
-
-        self.species = species
-        self.qty = qty
-        self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
+class AbstractMelonOrder():
+    """An abstract base class that other Melon Orders inherit from."""
 
     def get_total(self):
         """Calculate price, including tax."""
@@ -25,6 +15,19 @@ class DomesticMelonOrder():
         """Record the fact than an order has been shipped."""
 
         self.shipped = True
+
+
+class DomesticMelonOrder():
+    """A melon order within the USA."""
+
+    def __init__(self, species, qty):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = "domestic"
+        self.tax = 0.08
 
 
 class InternationalMelonOrder():
@@ -39,19 +42,6 @@ class InternationalMelonOrder():
         self.shipped = False
         self.order_type = "international"
         self.tax = 0.17
-
-    def get_total(self):
-        """Calculate price, including tax."""
-
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""

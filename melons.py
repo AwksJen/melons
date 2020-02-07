bb5bc87688769e0857ee60f3d0635f268089ea73
Your Name <you@example.com>
2020-02-07 10:01:33 -0800

    Part2 with super()

--- melons.py
+++ melons.py
@@ -4,7 +4,7 @@
 class AbstractMelonOrder():
     """An abstract base class that other Melon Orders inherit from."""
 
-    def __init__(self, species, qty, country_code, order_type, tax):
+    def __init__(self, species, qty, country_code, order_type, tax=None):
         """Initialize melon order attributes."""
 
         self.species = species
@@ -66,18 +66,18 @@ class InternationalMelonOrder(AbstractMelonOrder):
         return self.country_code
 
     def get_total(self):
+        total = super().get_total()
 
-        base_price = 5
+        # base_price = 5
         # if species == christmas melons
-        if self.species == "Christmas melons":
-            # multiple base price by 1.5
-            base_price = base_price * 1.5
-       
+        # if self.species == "Christmas melons":
+        #     # multiple base price by 1.5
+        #     base_price = base_price * 1.5
         if self.qty < 10:
             flat_fee = 3
-            total = (1 + self.tax) * self.qty * base_price + flat_fee
-        else:
-            total = (1 + self.tax) * self.qty * base_price
-
+            total = total + flat_fee
 
         return total
+
+
+# class GovernmentMelonOrder(AbstractMelonOrder):
